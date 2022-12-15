2e7154b239dfc463e13dda67c1c163fc8a826385
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-12-15 11:39:42 +0600

    added redirect functionality

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,45 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons"
 import { Link } from 'react-router-dom'
+import app from '../firebase'
 
 const Register = () => {
+    const auth = getAuth(app)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const emailFieldHandler = e => {
+        setEmail(e.target.value);
+    }
+
+    const passwordFieldHandler = e => {
+        setPassword(e.target.value);
+    }
+
+    const formHandler = e => {
+        e.preventDefault();
+
+        if (password.length < 6) {
+            setError('Password must be at least 6 characters.')
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setError('');
+            })
+            .catch(error => {
+                setError(error.message)
+            })
+    }
+
     return (
         <div className='h-screen'>
             <div className='px-6 py-12 h-full'>
                 <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
                     <div className="md:w-8/12 lg:w-4/12 mb-12 md:mb-0">
                         <h2 className='text-center text-3xl font-bold mb-5'>Create an account</h2>
-                        {/* <form> */}
-                        <div className="mb-6">
-                            <input
-                                type="text"
-                                className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                placeholder="Name"
-                            /></div>
 
-                        <div className="mb-6">
-                            <input
-                                type="text"
-                                className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                placeholder="Email address"
-                            /></div>
+                        <form onSubmit={formHandler}>
+                            <div className="mb-6">
+                                <input
+                                    type="text"
+                                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    placeholder="Name"
+                                /></div>
 
-                        <div className="mb-6">
-                            <input
-                                type="password"
-                                className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                placeholder="Password"
-                            />
-                        </div>
+                            <div className="mb-6">
+                                <input onBlur={emailFieldHandler}
+                                    type="email"
+                                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    placeholder="Email address"
+                                /></div>
 
-                        <button
-                            type="submit"
-                            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                            data-mdb-ripple="true"
-                            data-mdb-ripple-color="light">
-                            Register
-                        </button>
+                            <div className="mb-6">
+                                <input onBlur={passwordFieldHandler}
+                                    type="password"
+                                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                    placeholder="Password"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                                data-mdb-ripple="true"
+                                data-mdb-ripple-color="light">
+                                Register
+                            </button>
+                        </form>
 
                         <p className='text-center mt-4'>Already have an account? <Link to='/login' className='font-bold'>Login</Link></p>
 
